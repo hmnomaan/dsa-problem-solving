78286a7507a5fe0ebde3336831e54bd649059a51
Hasan Nomaan <hasannoman31@gmail.com>
2021-12-28 18:21:28 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// function sumNums(nums) {
+    
+//     for ( i = 0; i <= index.nums[i]; i++) {
+//         return index.nums[i] += i;
+//     }
+
+
+// }
+
+// console.log(sumNums([1,3,10]))
+
+
+
+function sumNums(Nums) {
+    
+    let result = 0;
+    for (num of Nums) {
+    
+        result += num;
+}
+    return result;
+
+}
+console.log(sumNums([1,3,10,50,21]))
